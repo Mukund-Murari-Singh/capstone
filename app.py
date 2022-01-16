1fdcfc6e796c6aab46f95556ab58e328fedf4056
Mukund-Murari-Singh <77293134+Mukund-Murari-Singh@users.noreply.github.com>
2022-01-16 14:59:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def home():
     output1.extend(['{}. {}'.format(i+1,overall_top5[i]) for i in range(5)])
     if request.method == 'POST':
         input1=request.form['username']
+        input1=input1.lower() #converting input to lowercase
         if re.search('[^\s]',input1) is None:
             flash('No username entered! Please enter a username & click "Submit".')
         elif input1 in list(clean_data.reviews_username):
@@ -40,24 +41,24 @@ def home():
         return render_template('index.html', placeholder_text=output1)
 
 def get_top5(input1,user_label,item_label,item_prediction,clean_data,vectorizer,sentiment_model):
-    input_user=user_label.transform([input1.lower()])[0]
+    input_user=user_label.transform([input1])[0] #convert to label encoded value
     #Generate top 20 recommended products from item-based recommendation system for this user
     #Top 20 items for input_user
-    pred20=pd.DataFrame(item_prediction.loc[:,input_user].sort_values(ascending=False)[0:20]).rename(columns={input_user:'predicted_sentiment'}).reset_index()
+    pred20=pd.DataFrame(item_prediction.loc[:,input_user].sort_values(ascending=False)[0:20]).rename(columns={input_user:'scoring'}).reset_index()
     pred20['item_name']=pred20.item.apply(lambda x:item_label.inverse_transform([x])[0])
     #Processing the top 20 recommended items' reviews for sentiment
     output_dict={'item_name':[],'total_reviews':[],'positive_fraction':[]}
     for item in pred20.item_name:
         positive_count=0
         total_reviews=0
-        for review in clean_data[clean_data.name==item].text_title_lemma:
+        for review in clean_data[clean_data.name==item].text_title_lemma: #fetching processed reviews for item
             total_reviews+=1
-            review_vec=vectorizer.transform([review])
-            if sentiment_model.predict(review_vec)[0]=='Positive':
+            review_vec=vectorizer.transform([review]) #vectorizing
+            if sentiment_model.predict(review_vec)[0]=='Positive': #counting predictions of positive sentiment
                 positive_count+=1
         output_dict['item_name'].append(item)
         output_dict['total_reviews'].append(total_reviews)
-        output_dict['positive_fraction'].append(round(positive_count/total_reviews,3))
+        output_dict['positive_fraction'].append(round(positive_count/total_reviews,3)) #calculating positive fraction of all reviews for item
     output=pd.DataFrame(output_dict).sort_values(by='positive_fraction',ascending=False)
     #Returning top 5 recommendations for input_user
     output_text=['The top 5 items recommended for user "{}" are:'.format(user_label.inverse_transform([input_user])[0])]
