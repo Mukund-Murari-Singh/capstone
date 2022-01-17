0ff996ab88055a0c731921d9d6195d5c704d5c8a
Mukund-Murari-Singh <77293134+Mukund-Murari-Singh@users.noreply.github.com>
2022-01-17 02:05:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,16 +26,6 @@ sentiment_model = pickle.load(open("pickle/sentiment_model.pickle", "rb")) #the 
 vectorizer = pickle.load(open("pickle/vectorizer.pickle", "rb")) #Tf-idf vectorizer to vectorize lemmatized text
 overall_top5 = pickle.load(open("pickle/overall_top5.pickle", "rb")) #overall top 5 items by poplarity & sentiment
 
-clean_data['item_code']=clean_data.name.apply(lambda x:item_label.transform([x])[0])
-clean_data['predicted_sentiment']=clean_data.text_title_lemma.apply(lambda x:sentiment_model.predict(vectorizer.transform([x]))[0])
-def get_frac(item_code):
-    try:
-        frac=round(clean_data[clean_data.item_code==item_code].predicted_sentiment.value_counts(normalize=True).loc['Positive'],3)
-    except:
-        frac=0
-    return frac
-pos_frac_dict={item_code:get_frac(item_code) for item_code in clean_data.item_code.unique()}
-
 def get_top5_2(input1):
     input_user=user_label.transform([input1])[0] #convert to label encoded value
     #Generate top 20 recommended products from item-based recommendation system for this user
@@ -59,7 +49,7 @@ def home():
         input1=request.form['username'] #fetching username input from app
         input1=input1.lower() #converting input to lowercase
         if input1 in list(clean_data.reviews_username.unique()): #checking if entered username is in database
-            output1=get_top5_2(input1) #,user_label,item_label,item_prediction,clean_data,vectorizer,sentiment_model)
+            output1=get_top5(input1) #,user_label,item_label,item_prediction,clean_data,vectorizer,sentiment_model)
         elif re.search('[^\s]',input1) is None: #checking if anything has been input or not
             flash('No username entered! Please enter a username & click "Submit".')
             output1=cold_start_output()
@@ -74,7 +64,7 @@ def get_top5(input1):#,user_label,item_label,item_prediction,clean_data,vectoriz
     input_user=user_label.transform([input1])[0] #convert to label encoded value
     #Generate top 20 recommended products from item-based recommendation system for this user
     #Top 20 items for input_user
-    pred20=pd.DataFrame(item_prediction.loc[:,input_user].sort_values(ascending=False)[0:20]).rename(columns={input_user:'scoring'}).reset_index()
+    pred20=pd.DataFrame(item_prediction.loc[:,input_user].sort_values(ascending=False)[0:20]).reset_index()
     pred20['item_name']=pred20.item.apply(lambda x:item_label.inverse_transform([x])[0])
     #Processing the top 20 recommended items' reviews for sentiment
     output_dict={'item_name':[],'total_reviews':[],'positive_fraction':[]}
@@ -91,7 +81,7 @@ def get_top5(input1):#,user_label,item_label,item_prediction,clean_data,vectoriz
         output_dict['positive_fraction'].append(round(positive_count/total_reviews,3)) #calculating positive fraction of all reviews for item
     output=pd.DataFrame(output_dict).sort_values(by='positive_fraction',ascending=False)
     #Returning top 5 recommendations for input_user
-    output_text=['The top 5 items recommended for user "{}" are:'.format(user_label.inverse_transform([input_user])[0])]
+    output_text=['The top 5 items recommended for user "{}" are:'.format(input1)]
     for number in range(5):
         output_text.append('{}. {}'.format(number+1,list(output.item_name)[number]))
     return output_text
